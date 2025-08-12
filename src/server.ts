b85e4f3b104251c721c7c1a626a4bb1f6a8d3b01
Rohit Gadhave <rohitgadhave249@gmail.com>
2025-08-12 10:00:23 +0530

    server static file send

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,19 @@ import express from "express";
 import { errorHandler } from "./middlewares/error.middleware";
 import { notFoundHandler } from "./middlewares/notFound.middleware";
 import { apiRouter } from "./routes/index.route";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// ✅ Fix __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.use('/api', apiRouter);
 
 app.use(notFoundHandler);
