13e9ff1d45ad009f825c63389d66529102eaf245
Rohit Gadhave <rohitgadhave249@gmail.com>
2025-08-24 20:00:54 +0530

    netlify setup

--- netlify/functions/app.ts
+++ netlify/functions/app.ts
@@ -1,4 +1,4 @@
 import serverless from "serverless-http";
-import app from "../../src/server";
-
+// import app from "../../src/server";
+import app from "../../dist/server";
 export const handler = serverless(app);
